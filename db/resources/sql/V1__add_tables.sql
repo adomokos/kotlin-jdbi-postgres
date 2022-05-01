d25b2617fc473cab6c7833a459a0242bfee765f9
Attila Domokos <adomokos@gmail.com>
2022-05-01 10:24:31 -0500

    Add the phone_numbers table

--- db/resources/sql/V1__add_tables.sql
+++ db/resources/sql/V1__add_tables.sql
@@ -2,3 +2,9 @@ CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   name text not null
 );
+
+CREATE TABLE phone_numbers (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id) NOT NULL,
+  phone_number text NOT null
+)
